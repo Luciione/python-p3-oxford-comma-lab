9f748e5088c2c8b597c7a36260c25eb0aefb59ca
+ <lucy.njoroge1@student.moringaschool.com>
2023-08-24 01:11:22 +0300

    complete

--- lib/oxford_comma.py
+++ lib/oxford_comma.py
@@ -1,2 +1,9 @@
-def oxford_comma(items):
-    return None
+def oxford_comma(array):
+    if len(array) == 1:
+        return array[0]
+    elif len(array) == 2:
+        return f"{array[0]} and {array[1]}"
+    else:
+        last_element = array[-1]
+        other_elements = ", ".join(array[:-1])
+        return f"{other_elements}, and {last_element}"
